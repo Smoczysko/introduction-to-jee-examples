eccde15f9f40029d0f1b2280e786e660a851b074
lukasz.rybka@solwit.pl <lukasz.rybka@solwit.pl>
2014-12-07 14:58:01 +0100

    JPA entity lifecycle events example.

---
+++ jpa/lifecycle-events/src/main/java/pl/edu/ug/introductiontojee/jpa/ejb/domain/Message.java
@@ -0,0 +1,88 @@
+package pl.edu.ug.introductiontojee.jpa.ejb.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "message.all", query = "select m from Message m order by m.id")
+})
+public class Message implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String author;
+    private String title;
+    private String message;
+
+    @Temporal(TemporalType.DATE)
+    private Date createdAt;
+
+    @Temporal(TemporalType.DATE)
+    private Date modifiedAt;
+
+    public Message() {
+        super();
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date date) {
+        this.createdAt = date;
+    }
+
+    public Date getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(Date modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+        this.modifiedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    void modifiedAt() {
+        this.modifiedAt = new Date();
+    }
+}
---
+++ jpa/lifecycle-events/src/main/java/pl/edu/ug/introductiontojee/jpa/ejb/web/ViewMessageServlet.java
@@ -0,0 +1,25 @@
+package pl.edu.ug.introductiontojee.jpa.ejb.web;
+
+import pl.edu.ug.introductiontojee.jpa.ejb.domain.MessageStorageService;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/view/*")
+public class ViewMessageServlet extends HttpServlet{
+    @EJB
+    private MessageStorageService storage;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Long messageID = WebUtils.getMessageID(request);
+
+        request.setAttribute("message", storage.get(messageID));
+        request.getRequestDispatcher("/message/view.jsp").forward(request, response);
+    }
+}
