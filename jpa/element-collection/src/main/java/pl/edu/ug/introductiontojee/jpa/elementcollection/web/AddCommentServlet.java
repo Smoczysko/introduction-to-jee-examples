f5cde1408a82ead29d59dce0762b21c9f396569a
lukasz.rybka@solwit.pl <lukasz.rybka@solwit.pl>
2014-12-13 18:54:59 +0100

    JPA element collection instead of one-to-many rellation example.

---
+++ jpa/element-collection/src/main/java/pl/edu/ug/introductiontojee/jpa/elementcollection/web/AddCommentServlet.java
@@ -0,0 +1,30 @@
+package pl.edu.ug.introductiontojee.jpa.elementcollection.web;
+
+import pl.edu.ug.introductiontojee.jpa.elementcollection.domain.Message;
+import pl.edu.ug.introductiontojee.jpa.elementcollection.domain.MessageStorageService;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/comments/add/*")
+public class AddCommentServlet extends HttpServlet{
+    @EJB
+    private MessageStorageService messageStorage;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Long messageID = WebUtils.getID(request);
+        Message message = messageStorage.get(messageID);
+
+        String comment = request.getParameter("message");
+
+        messageStorage.addComment(message, comment);
+
+        response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/view/" + messageID));
+    }
+}
---
+++ jpa/element-collection/src/main/webapp/parts/navbar.jsp
@@ -0,0 +1,7 @@
+<div class="navbar navbar-inverse" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="${pageContext.request.contextPath}/">Message board</a>
+        </div>
+    </div>
+</div>
