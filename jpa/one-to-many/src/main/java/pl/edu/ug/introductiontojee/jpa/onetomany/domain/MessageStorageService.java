c54cd5312d88eea84f553c20bc3d108ebe7d1c28
lukasz.rybka@solwit.pl <lukasz.rybka@solwit.pl>
2014-12-13 16:30:29 +0100

    JPA One-to-Many relation example.

---
+++ jpa/one-to-many/src/main/java/pl/edu/ug/introductiontojee/jpa/onetomany/domain/MessageStorageService.java
@@ -0,0 +1,38 @@
+package pl.edu.ug.introductiontojee.jpa.onetomany.domain;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Date;
+import java.util.List;
+
+@Stateless
+public class MessageStorageService {
+    @PersistenceContext
+    EntityManager em;
+
+    public Message get(Long id) {
+        return em.find(Message.class, id);
+    }
+
+    public void add(Message message) {
+        message.setId(null);
+        message.setCreatedAt(new Date());
+
+        em.persist(message);
+    }
+
+    public void addComment(Message message, Comment comment) {
+        List<Comment> comments = message.getComments();
+        comments.add(comment);
+
+        message.setComments(comments);
+
+        em.merge(message);
+    }
+
+    public List<Message> getAllMessages(){
+        //noinspection unchecked
+        return em.createNamedQuery("message.all").getResultList();
+    }
+}
